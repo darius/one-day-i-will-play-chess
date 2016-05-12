481e3d30d588f413fbffce557b375ccd03c00150
Darius Bacon <darius@wry.me>
2016-05-11 21:03:15 -0400

    start, derived from sketchbook/chess

---
+++ chack.py
@@ -0,0 +1,375 @@
+"""
+Chess board
+No computer player yet
+Sucks in other ways too
+"""
+
+import json
+
+## response = '{"clock":{"w":300,"b":300},"pgn":"","fen":"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1","turn":"w","result":"*","color":"w"}'
+## rr = json.loads(response)
+## rr
+#. {u'turn': u'w', u'pgn': u'', u'clock': {u'b': 300, u'w': 300}, u'fen': u'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', u'result': u'*', u'color': u'w'}
+## rr['clock']
+#. {u'b': 300, u'w': 300}
+## rr['turn']
+#. u'w'
+## print str(parse_FEN(rr['fen']))
+#. rnbqkbnr
+#. pppppppp
+#.         
+#.         
+#.         
+#.         
+#. PPPPPPPP
+#. RNBQKBNR
+
+## b = InitialChessBoard()
+## b.mover
+#. 'white'
+## b.castled
+#. AttributeError: ChessBoard instance has no attribute 'castled'
+## print str(b)
+#. rnbqkbnr
+#. pppppppp
+#.         
+#.         
+#.         
+#.         
+#. PPPPPPPP
+#. RNBQKBNR
+## pw = HumanPlayer(white)
+## pb = HumanPlayer(black)
+## b.outcome
+## ' '.join(sorted(map(str, b.get_moves())))
+#. 'a2a3 a2a4 b1a3 b1c3 b2b3 b2b4 c2c3 c2c4 d2d3 d2d4 e2e3 e2e4 f2f3 f2f4 g1f3 g1h3 g2g3 g2g4 h2h3 h2h4 resign'
+## m = b.parse_move('resign')
+## b1 = m.update(b)
+## b1.outcome
+#. 'black'
+
+## fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+## b2 = parse_FEN(fen)
+## b2.mover
+#. 'white'
+## b2.castling
+#. (True, True)
+## print str(b2)
+#. rnbqkbnr
+#. pppppppp
+#.         
+#.         
+#.         
+#.         
+#. PPPPPPPP
+#. RNBQKBNR
+
+def parse_FEN(fen):
+    placement, to_move, castling, en_passant_target, halfmove_clock, fullmove_clock = fen.split()
+    squares = []
+    for row in placement.split('/'):
+        s = ''
+        for p in row:
+            if p.isdigit():
+                s += ' ' * int(p)
+            else:
+                s += p
+        squares.append(s)
+    mover = {'w': 'white', 'b': 'black'}[to_move]
+    if castling == '-':
+        castling = (False, False)
+    else:
+        castling = ('k' in castling or 'q' in castling,
+                    'K' in castling or 'Q' in castling)
+    en_passant = None if en_passant_target == '-' else parse_coords(en_passant_target)
+    return ChessBoard(mover, surround(squares), castling, en_passant, None)
+
+def surround(rows):
+    return ['----------'] + ['-%s-' % row for row in rows] + ['----------']
+
+def main():
+    print "(Moves look like 'e2e3')"
+    play_chess(HumanPlayer, HumanPlayer)
+
+def play_chess(white_strategy, black_strategy):
+    return play(InitialChessBoard(), [white_strategy, black_strategy])
+
+def play(board, strategies):
+    players = [strategy(side)
+               for strategy, side in zip(strategies, board.get_sides())]
+    while board.get_outcome() is None:
+        board = board.play_turn(players)
+    for player in players:
+        player.on_game_over(board)
+
+class HumanPlayer:
+
+    def __init__(self, side):
+        self.side = side
+
+    def pick_move(self, board):
+        board.show()
+        while True:
+            string = raw_input('%s, your move? ' % self.side.capitalize())
+            try:
+                move = board.parse_move(string)
+            except MoveIllegal:
+                print 'Illegal move.'
+            else:
+                return move
+
+    def on_game_over(self, board):
+        board.show()
+        if board.get_outcome() is None:
+            pass
+        elif board.get_outcome() == self.side:
+            print '%s, you win!' % self.side.capitalize()
+        elif board.get_outcome() == 'draw':
+            print 'You draw.'
+        else:
+            print '%s, you lose!' % self.side.capitalize()
+
+def InitialChessBoard():
+    squares = ['----------',
+               '-rnbqkbnr-',
+               '-pppppppp-',
+               '-        -',
+               '-        -',
+               '-        -',
+               '-        -',
+               '-PPPPPPPP-',
+               '-RNBQKBNR-',
+               '----------',]
+    return ChessBoard(white, squares, (False, False), None, None)
+
+class MoveIllegal(Exception):
+    pass
+
+class ChessBoard:
+
+    def __init__(self, mover, squares, castling, en_passant_target, outcome):
+        self.mover = mover
+        self.squares = squares
+        self.castling = castling
+        self.en_passant_target = en_passant_target
+        self.outcome = outcome
+
+    def __str__(self):
+        return '\n'.join(line[1:-1] for line in self.squares[1:-1])
+
+    def can_castle(self, player):
+        return self.castling[player == black]
+
+    def get_outcome(self):
+        "Return None, 'draw', black, or white (meaning the winner)."
+        return self.outcome
+
+    def resign(self):
+        return ChessBoard(opponent(self.mover),
+                          self.squares,
+                          self.castling,
+                          None,
+                          opponent(self.mover))
+
+    def move_piece(self, (r0, c0), (r1, c1), en_passant_target=None):
+        squares = list(map(list, self.squares))
+        piece = squares[r0][c0]
+        squares[r0][c0] = ' '
+        squares[r1][c1] = piece
+        return ChessBoard(opponent(self.mover),
+                          list(map(''.join, squares)),
+                          self.castling,
+                          en_passant_target,
+                          None)
+
+    def move_en_passant(self, (r0, c0), (r1, c1)):
+        squares = list(map(list, self.squares))
+        piece = squares[r0][c0]
+        squares[r0][c0] = ' '   # moving from here...
+        squares[r0][c1] = ' '   # (capturing this pawn)
+        squares[r1][c1] = piece # ... to there.
+        return ChessBoard(opponent(self.mover),
+                          list(map(''.join, squares)),
+                          self.castling,
+                          None,
+                          None)
+
+    def move_promoting(self, (r0, c0), (r1, c1)):
+        # XXX support promotions other to queen 
+        squares = list(map(list, self.squares))
+        piece = squares[r0][c0]
+        squares[r0][c0] = ' '
+        squares[r1][c1] = 'Q' if self.mover == 'white' else 'q'
+        return ChessBoard(opponent(self.mover),
+                          list(map(''.join, squares)),
+                          self.castling,
+                          None,
+                          None)
+
+    def show(self):
+        print self
+
+    def get_sides(self):
+        return (white, black)
+
+    def play_turn(self, (white_player, black_player)):
+        player = white_player if self.mover == white else black_player
+        move = player.pick_move(self)
+        if move in self.get_moves():
+            return move.update(self)
+        raise Exception("Bad move")
+
+    def parse_move(self, string):
+        for move in self.get_moves():
+            if move.matches(string):
+                return move
+        raise MoveIllegal()
+
+    def get_moves(self):
+        return [ResignMove()] + self.get_piece_moves()
+
+    def get_piece_moves(self):
+        return sum(map(self.moves_from, self.army(self.mover)), [])
+
+    def army(self, player):
+        for r, row in enumerate(self.squares):
+            for c, piece in enumerate(row):
+                if piece.isalpha() and piece.isupper() == (player == white):
+                    yield r, c
+
+    def moves_from(self, pos):
+        return list(self.gen_moves_from(pos))
+
+    def gen_moves_from(self, (r, c)):
+        piece = self.squares[r][c]
+        piece, is_white = piece.upper(), piece.isupper()
+
+        def is_takeable(r1, c1):
+            return is_empty(r1, c1) or has_opponent(r1, c1)
+
+        def is_empty(r1, c1):
+            return self.squares[r1][c1] == ' '
+
+        def has_opponent(r1, c1):
+            there = self.squares[r1][c1]
+            return there.isalpha() and there.isupper() != is_white
+
+        def move_to(r1, c1):
+            return PieceMove((r, c), (r1, c1))
+
+        def pawn_move(r1, c1, en_passant_target=None):
+            back_rank = 1 if is_white else 8
+            if r1 == back_rank:
+                return PawnPromotion((r, c), (r1, c1))
+            else:
+                return PieceMove((r, c), (r1, c1), en_passant_target)
+
+        def move_freely(dirs):
+            for dr, dc in dirs:
+                for i in range(1, 9):
+                    if is_empty(r+dr*i, c+dc*i):
+                        yield pawn_move(r+dr*i, c+dc*i)
+                    else:
+                        if has_opponent(r+dr*i, c+dc*i):
+                            yield pawn_move(r+dr*i, c+dc*i)
+                        break
+
+        if piece in ' -':
+            pass
+        elif piece == 'P':
+            forward = -1 if is_white else 1
+            if is_empty(r+forward, c):
+                yield move_to(r+forward, c)
+                if r == (7 if is_white else 2): # initial 2 steps
+                    if is_empty(r+forward*2, c):
+                        yield pawn_move(r+forward*2, c, en_passant_target=(r+forward, c))
+            if has_opponent(r+forward, c-1): yield move_to(r+forward, c-1)
+            if has_opponent(r+forward, c+1): yield move_to(r+forward, c+1)
+            if self.en_passant_target:
+                er, ec = self.en_passant_target
+                if er == r+forward and abs(ec-c) == 1:
+                    yield EnPassantCapture((r, c), (er, ec))
+        elif piece == 'K':
+            # TODO castling
+            # TODO forbid moving into check
+            # (and this can apply to moves of other pieces)
+            for dr, dc in queen_dirs:
+                if is_takeable(r+dr, c+dc):
+                    yield move_to(r+dr, c+dc)
+        elif piece == 'Q':
+            for move in move_freely(queen_dirs):  yield move
+        elif piece == 'R':
+            for move in move_freely(rook_dirs):   yield move
+        elif piece == 'B':
+            for move in move_freely(bishop_dirs): yield move
+        elif piece == 'N':
+            for dr, dc in knight_jumps:
+                if 1 <= r+dr <= 8 and 1 <= c+dc <= 8:
+                    if is_takeable(r+dr, c+dc):
+                        yield move_to(r+dr, c+dc)
+        else:
+            assert False
+
+rook_dirs   = [( 0, 1), ( 0,-1), ( 1, 0), (-1, 0)]
+bishop_dirs = [(-1,-1), (-1, 1), ( 1,-1), ( 1, 1)]
+queen_dirs  = rook_dirs + bishop_dirs
+
+knight_jumps = [( 2, 1), ( 2,-1), ( 1, 2), ( 1,-2),
+                (-2, 1), (-2,-1), (-1, 2), (-1,-2)]
+
+white, black = 'white', 'black'
+
+def opponent(side):
+    return black if side == white else white
+
+class ResignMove:
+    def __eq__(self, other):
+        return isinstance(other, ResignMove)
+    def update(self, board):
+        return board.resign()
+    def matches(self, string):
+        return string.lower() == 'resign'
+    def matches(self, string):
+        return string.lower() == str(self)
+    def __str__(self):
+        return 'resign'
+
+class PieceMove:
+    def __init__(self, from_pos, to_pos, en_passant_target=None):
+        self.from_pos = from_pos
+        self.to_pos   = to_pos
+        self.en_passant_target = en_passant_target
+    def __eq__(self, other):
+        return (isinstance(other, PieceMove)
+                and self.from_pos == other.from_pos
+                and self.to_pos == other.to_pos
+                and self.en_passant_target == other.en_passant_target)
+    def update(self, board):
+        return board.move_piece(self.from_pos, self.to_pos, self.en_passant_target)
+    def matches(self, string):
+        return string.lower() == str(self)
+    def __str__(self):
+        fr, fc = self.from_pos
+        tr, tc = self.to_pos
+        return '%s%d%s%d' % ('abcdefgh'[fc-1], 9-fr,
+                             'abcdefgh'[tc-1], 9-tr)
+
+def parse_coords(s):
+    assert len(s) == 2
+    c = 1 + 'abcdefgh'.index(s[0])
+    r = int(s[1])
+    assert 1 <= r <= 8
+    return r, c
+
+class EnPassantCapture(PieceMove):
+    # XXX might need special parsing/unparsing
+    def update(self, board):
+        return board.move_en_passant(self.from_pos, self.to_pos)
+
+class PawnPromotion(PieceMove):
+    # XXX might need special parsing/unparsing
+    def update(self, board):
+        return board.move_promoting(self.from_pos, self.to_pos)
+
+if __name__ == '__main__':
+    main()
